b0011ba81c2e5de688ed745c225856346df12cc2
PC69 <PC69@3CT45>
2015-10-23 20:30:10 -0500

    Versión Paneles

--- web/js/indexController.js
+++ web/js/indexController.js
@@ -1,7 +1,24 @@
 Ext.onReady(function() {
 
-    var panel = new Ext.Panel({
-      title:"Titulo",
+    var panelNorte = new Ext.Panel({
+      title:"Titulo Norte",
+      region:"north"
+    });
+    var panelSur = new Ext.Panel({
+      title:"Titulo Sur",
+      region:"south"
+    });
+    var panelEste = new Ext.Panel({
+      title:"Titulo Este",
+      region:"east"
+    });
+    var panelOeste = new Ext.Panel({
+      title:"Titulo Oeste",
+      region:"west"
+    });
+
+    var panelCentro = new Ext.Panel({
+      title:"Titulo Centro",
       region:"center"
     });
 
@@ -10,7 +27,11 @@ Ext.onReady(function() {
         layout: 'border',
         items: [
             
-                panel
+                panelNorte,
+                panelSur,
+                panelCentro,
+                panelEste,
+                panelOeste
             
         ]
     });
